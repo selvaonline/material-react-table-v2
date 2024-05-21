210819bfb95d85e4c2f98dd013dea9c3c45568a9
Selvakumar Murugesan <selvaonline@gmail.com>
2024-05-21 18:57:20 -0500

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+
+
+
+const errorString = 'Record already mapped to a Classification - 330B Accounts |122|1649|University of Dallas Texas';
+
+const extractedData =
+  errorString.match(/Record already mapped to a Classification - (.+?)(\s*\|[^|]+)+\|(.+?)$/)?.[1] + ' - ' +
+  errorString.match(/Record already mapped to a Classification - (.+?)(\s*\|[^|]+)+\|(.+?)$/)?.[3];
+
+console.log(extractedData
